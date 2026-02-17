a6ef29efb9b823531b00b3db979eb932b7b3b8f3
Henrique Martin - Martin Projetos <128996059+henriquemmartin@users.noreply.github.com>
2026-02-17 11:25:22 -0300

    Update conn.js

--- conn.js
+++ conn.js
@@ -3,10 +3,7 @@ require('dotenv').config();
 async function main() {
   const uri = process.env.MONGODB_URI
  
-//bando de dados_usaername henriquemmartin senha lYIJj2cp1bUmhXoT
-//mongodb://mongo:YRYkGEucVKOtPSqTGfQgzCdpSZzQcaZm@autorack.proxy.rlwy.net:14111
-//bando de dados teste username henriquemmartin senha RiluhWPjASwVcsC5
-//mongodb+srv://henriquemmartin:RiluhWPjASwVcsC5@cluster0.dkvnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
   await mongoose.connect(uri);
   console.log("conectado ao bando de dados Primario!");
 }
